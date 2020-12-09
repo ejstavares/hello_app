f5a23ce8ae353097e8000d6a772227e6324f0938
ejstavares <ederlino.dev@gmail.com>
2020-12-09 19:03:52 -0100

    unit test

--- test/visit_service_test.dart
+++ test/visit_service_test.dart
@@ -1,30 +1,56 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  test("get all visit with success", () {
+  test("http get all visit with success", () async {
     //setup
 
     //act
+    var response = await http
+        .get('https://5faaf775dbbef70016d47f6f.mockapi.io/api/v1/tasks/10000');
+
+    print("response statusCode: ${response.statusCode} ");
+    print("response: ${response.body}");
 
     //assert
-
+    expect(response.statusCode, 200);
   });
 
-  test("get all visit with success", () {
+  test("httpClient get all visit with success", () async {
     //setup
 
     //act
+    var client = HttpClient();
+
+    Future<HttpClientRequest> request = client.get(
+        "5faaf775dbbef70016d47f6f.mockapi.io", null, "api/v1/tasks/1");
+
+    var r = await request;
+
+    var response = await r.close();
+
+    print("response::: ${response.statusCode}");
 
     //assert
-
+    expect(response.statusCode, 200);
   });
 
-  test("get all visit with success", () {
+  test("dio get all visit with success", () async {
     //setup
 
     //act
+    var dio = Dio();
+    dio.options.baseUrl = "https://5faaf775dbbef70016d47f6f.mockapi.io";
+    // dio.options.headers = "https://5faaf775dbbef70016d47f6f.mockapi.io";
+
+    Response response = await dio.get("/api/v1/tasks/1");
+
+    print("response dio::: ${response.data}");
 
     //assert
-
+    expect(response.statusCode, 200);
   });
 }
