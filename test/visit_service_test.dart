cb0e7d185fa2f07ebe198ec7bf0daab0917aeee1
ejstavares <ederlino.dev@gmail.com>
2020-12-09 18:00:55 -0100

    init test

---
+++ test/visit_service_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("get all visit with success", () {
+    //setup
+
+    //act
+
+    //assert
+
+  });
+
+  test("get all visit with success", () {
+    //setup
+
+    //act
+
+    //assert
+
+  });
+
+  test("get all visit with success", () {
+    //setup
+
+    //act
+
+    //assert
+
+  });
+}
